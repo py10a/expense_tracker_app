932e1fdae801ad3b2c69625d23256f621fe8d5ea
Andrew <andriypylio@gmail.com>
2023-11-27 00:29:07 +0100

    Create card-type view for Expense item.

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,18 +1,35 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
-enum Category { food, bills, clothes, entertainment, other }
+/// `Category` represents the category of an expense.
+enum Category {
+  food,
+  bills,
+  clothes,
+  entertainment,
+  other,
+}
 
-/// `Expense` represents an individual expense item.
-///
-/// Each `Expense` has a unique [id], a [name], a [cost], a [category], and a [date].
-/// The [id] unique identifier for each expense, generated using a UUID.
-/// The [name] name of the expense.
-/// The [cost]  cost of the expense.
-/// The [category] category of the expense, which defaults to [Category.other] if not specified.
-/// The [date]  date of the expense, defaults is `DateTime.now()`.
+/// `categoryIcons` is a map of `Category` to `IconData`.
+const Map<Category, IconData> categoryIcons = {
+  Category.food: Icons.fastfood,
+  Category.bills: Icons.money,
+  Category.clothes: Icons.shopping_bag,
+  Category.entertainment: Icons.movie,
+  Category.other: Icons.category,
+};
+
 class Expense {
+  /// `Expense` represents an individual expense item.
+  ///
+  /// Each `Expense` has a unique [id], a [name], a [cost], a [category], and a [date].
+  /// The [id] unique identifier for each expense, generated using a UUID.
+  /// The [name] name of the expense.
+  /// The [cost]  cost of the expense.
+  /// The [category] category of the expense, which defaults to [Category.other] if not specified.
+  /// The [date]  date of the expense, defaults is `DateTime.now()`.
   Expense({
     required this.name,
     required this.cost,
@@ -26,4 +43,9 @@ class Expense {
   final double cost;
   final Category category;
   final DateTime date;
+
+  /// Returns a formatted date string in the format of `DD.MM.YYYY`.
+  String get formattedDate {
+    return '${date.day}.${date.month}.${date.year}';
+  }
 }
--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/models/expense.dart';
 
 class ExpenseItem extends StatelessWidget {
+  /// `ExpenseItem` is a widget that displays an individual expense item.
   const ExpenseItem({Key? key, required this.expense}) : super(key: key);
 
   final Expense expense;
@@ -12,10 +13,45 @@ class ExpenseItem extends StatelessWidget {
     return Card(
       child: Padding(
         padding: const EdgeInsets.symmetric(
-          vertical: 32.0,
-          horizontal: 32.0,
+          vertical: 18.0,
+          horizontal: 18.0,
         ),
-        child: Text(expense.name),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              expense.name,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Row(
+              children: [
+                Text(
+                  '\$ ${expense.cost.toStringAsFixed(2)}',
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(
+                      categoryIcons[expense.category],
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(width: 8.0),
+                    Text(
+                      expense.formattedDate,
+                      style: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
