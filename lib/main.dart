5c4b13852a38adfcddea83b15041c40abe85b307
Andrew <andriypylio@gmail.com>
2023-11-29 01:19:02 +0100

    Add color styling for the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+
 import 'package:expense_tracker_app/widgets/expenses.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      title: 'Expense Tracker', // used by the OS task switcher
-      color: Colors.cyan, // color of the app in the OS task switcher
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
-        fontFamily: 'Quicksand',
+      title: 'Expense Tracker',
+      color: const Color.fromARGB(255, 218, 167, 227),
+      theme: ThemeData.light().copyWith(
+        primaryColor: const Color.fromARGB(255, 218, 167, 227),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 238, 221, 240),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 218, 167, 227),
+          foregroundColor: Color.fromARGB(255, 252, 232, 255),
+          elevation: 0.0,
+          iconTheme: IconThemeData(
+            color: Color.fromARGB(255, 252, 232, 255),
+          ),
+        ),
       ),
       home: const Expenses(),
     ),
